af8197286ec3bec12de8e37446f7cfb0ae352653
Cyril Yen <cyril_yen@bizlinktech.com>
2018-08-03 14:12:32 +0800

    s\update

---
+++ Module/db_base.py
@@ -0,0 +1,34 @@
+import pymysql
+
+# 資料庫連線模組
+class Db_base():
+
+	def __init__(self):
+		self.db = pymysql.connect(
+				"xxx.ckgoiwrtkuxz.ap-northeast-1.rds.amazonaws.com",
+				"xxx`",
+				"xxx",
+				"spider1")
+
+	# dispose
+	def dispose(self):
+		self.db.close()
+
+    # excute
+	def excute(self, query):
+		cursor = self.db.cursor()
+		cursor.execute(query)
+		result = cursor.fetchone()
+		return result
+
+	# excutemay
+	def executemany(self, query, params):
+		cursor = self.db.cursor()
+		cursor.executemany(query, params)
+
+	# execute and fetch all
+	def executeFetchAll(self, query):
+		cursor = self.db.cursor()
+		cursor.execute(query)
+		return cursor.fetchall()
+
